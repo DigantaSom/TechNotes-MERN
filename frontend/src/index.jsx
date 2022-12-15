93e217d0b97c2be9c7b8d2b973461395a6e98dd8
Diganta Som <itsdsom111@gmail.com>
2022-12-15 20:36:28 +0530

    Frontend - Using Redux-Toolkit to fetch Users and Notes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { store } from './app/store';
 
 import App from './App';
 
@@ -9,10 +12,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
